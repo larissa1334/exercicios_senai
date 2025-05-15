f224e5080847ba82f740f0e8fdde95cb4f0ecc1f
larissa1334 <larissaizabel641@gmail.com>
2025-05-15 15:39:23 -0300

    Add files via upload

---
+++ diadasemana.py
@@ -0,0 +1,24 @@
+dia = (input('digite o dia '))
+
+if dia == '1':
+    print('domingo')
+
+elif dia =='2':
+    print('segunda')
+
+elif dia =='3':
+    print('terça')
+
+elif dia == '4':('quarta')
+
+elif dia == '5':
+    print ('quinta')   
+
+elif dia == '6':
+    print('sexta')
+    
+elif dia == '7':
+    print('sabado')   
+
+else: 
+    print ('numero errado')
