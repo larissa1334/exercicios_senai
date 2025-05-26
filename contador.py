01155b3d67b155c16232e0c9145ffe3606911cd1
larissa1334 <larissaizabel641@gmail.com>
2025-05-26 15:51:41 -0300

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,18 @@
+#numero= int (input('digite um numero'))
+#a=0
+
+#while a <= numero:
+ #   print(a)
+  #  a= a+1
+
+senha= input('digite a senha:')
+
+if senha !='tartaruga':
+
+    while senha !='tartaruga':
+        print('senha incorreta')
+        senha=input('digite a senha: ')
+
+print('senha correta')
+    
+
