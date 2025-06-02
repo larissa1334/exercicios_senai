f5b79c3cb357547c8f5434202d7be37f0bc12bcb
larissa1334 <larissaizabel641@gmail.com>
2025-06-02 14:22:46 -0300

    Add files via upload

---
+++ tenis.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+#----------------------------------------------SIDEBAR
+st.sidebar.title('marcas')
+
+marcas={'rainha','jordan','converse','vans','nike','puma','adidas'}
+
+st.sidebar.selectbox('selecione uma marca',marcas)
+#-----------------------------------------------principal
+st.subheader('TENÌS')
+st.subheader('IMAGEM')
+st.subheader('MARCAS')
+
+with 
+
+marcas=['rainha','jordan','converse','vans','nike','puma','adidas']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
