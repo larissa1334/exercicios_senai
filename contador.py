72db6a36bd2855cfd87a7b531642f23130cde8c4
larissa1334 <larissaizabel641@gmail.com>
2025-06-04 15:17:02 -0300

    Add files via upload

--- contador.py
+++ contador.py
@@ -1,18 +1,6 @@
-#numero= int (input('digite um numero'))
-#a=0
+contador=0
+fim=int(input('vou contar ate o numero que voce pedir.digite um numwero:'))
 
-#while a <= numero:
- #   print(a)
-  #  a= a+1
-
-senha= input('digite a senha:')
-
-if senha !='tartaruga':
-
-    while senha !='tartaruga':
-        print('senha incorreta')
-        senha=input('digite a senha: ')
-
-print('senha correta')
-    
-
+while contador<=fim:
+    print (contador)
+    contador=contador+1
