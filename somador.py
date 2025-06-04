ba6b84c6b3286f2e83748e4e1690f191b8d30324
larissa1334 <larissaizabel641@gmail.com>
2025-06-04 15:17:23 -0300

    Add files via upload

---
+++ somador.py
@@ -0,0 +1,9 @@
+contador=0
+fim=int(input('vou somar ate o numero que vc pedir.digite um numero:'))
+soma=10
+
+while contador<=fim:
+ soma =soma + contador
+ contador= contador+1
+
+print(f'resultado:{soma}')
