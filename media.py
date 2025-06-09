c04635b659b95b19940441c3684874c76fe63e57
larissa1334 <larissaizabel641@gmail.com>
2025-06-09 14:02:13 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,9 @@
+nome = input ('nome do aluno: ')
+numero_1 = float(input('altura:') )
+numero_2 = float(input('' ))
+numero_3 = float(input('digite a terceira nota:' ))
+media = (numero_1+numero_2+numero_3) / 3
+print (nome)
+print ("Média",media)
+print('aluno:', nome,' \nMédia:' ,media )
+print('Aluno:', nome, ' \nMédia:,media')
